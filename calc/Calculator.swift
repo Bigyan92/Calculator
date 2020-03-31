bdd855210eb7690a45e8d7b6521b82fcec0a68e0
Jack Song <133342301@163.com>
2020-03-31 22:53:01 +1100

    Minor updates to comments

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class Calculator {
     
+    /// For multi-step calculation, it's helpful to persist existing result
     var currentResult = 0;
     
     /// Perform Addition
@@ -24,5 +25,13 @@ class Calculator {
     /// - SeeAlso: https://developer.apple.com/documentation/swift/int/2884663-addingreportingoverflow
     func add(no1: Int, no2: Int) -> Int {
         return no1 + no2;
+    }
+    
+    func calculate(args: [String]) -> String {
+        // Todo: Calculate Result from the arguments. Replace dummyResult with your actual result;
+        let dummyResult = add(no1: 1, no2: 2);
+        
+        let result = String(dummyResult);
+        return(result)
     }
 }
